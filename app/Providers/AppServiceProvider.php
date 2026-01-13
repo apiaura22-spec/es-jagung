6d34059e33a737537e81d861868a3435b73afb99
Muhd Esa Aksara Pratama <muhdesapratama05@gmail.com>
2026-01-13 20:00:46 +0700

    fix: force https for images

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,8 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-    if (config('app.env') === 'production') {
-        \Illuminate\Support\Facades\URL::forceScheme('https');
-    }
+        // Memaksa HTTPS jika aplikasi berjalan di server (bukan di laptop/lokal)
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
